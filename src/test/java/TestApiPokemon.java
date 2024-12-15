426c7565c7d2fa29f8604594007b4867b480bc82
Filipe Arruda <filipehenriquester@gmail.com>
2024-12-15 17:24:29 -0300

    New tests

--- src/test/java/TestApiPokemon.java
+++ src/test/java/TestApiPokemon.java
@@ -100,4 +100,68 @@ public class TestApiPokemon {
                         .extract().response();
         System.out.println(responseAbility.asString());
     }
+
+    @Test
+    public void test5() {
+        Response response =
+                given()
+                        .baseUri("https://pokeapi.co")
+                        .basePath("/api/v2/pokemon/ditto")
+                        .header("Content-Type", "application/json")
+                        .header("Accept", "*/*")
+                        .when()
+                        .get()
+                        .then()
+                        .assertThat()
+                        .statusCode(200)
+                        .extract().response();
+
+        String ability = response.jsonPath().getString("abilities.ability.name[0]");
+
+        Response responseAbility =
+                given()
+                        .baseUri("https://pokeapi.co")
+                        .basePath("api/v2/ability/" + ability)
+                        .header("Content-Type", "application/json")
+                        .header("Accept", "*/*")
+                        .when()
+                        .get()
+                        .then()
+                        .assertThat()
+                        .statusCode(200)
+                        .extract().response();
+        System.out.println(responseAbility.asString());
+    }
+
+    @Test
+    public void test6() {
+        Response response =
+                given()
+                        .baseUri("https://pokeapi.co")
+                        .basePath("/api/v2/pokemon/ditto")
+                        .header("Content-Type", "application/json")
+                        .header("Accept", "*/*")
+                        .when()
+                        .get()
+                        .then()
+                        .assertThat()
+                        .statusCode(200)
+                        .extract().response();
+
+        response.jsonPath().getList("abilities.ability.name").forEach(ability -> {
+            Response responseAbility =
+                    given()
+                            .baseUri("https://pokeapi.co")
+                            .basePath("api/v2/ability/" + ability)
+                            .header("Content-Type", "application/json")
+                            .header("Accept", "*/*")
+                            .when()
+                            .get()
+                            .then()
+                            .assertThat()
+                            .statusCode(200)
+                            .extract().response();
+            System.out.println(responseAbility.asString());
+        });
+    }
 }
